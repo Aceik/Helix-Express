a0c3bbcfc7ec31a24c256eaa2c9f90cc57e9f8e0
Thomas Tyack <thomastyack@aceik.com.au>
2017-09-19 00:06:47 +1000

    a few more helix express updates

--- template/helix-express.js
+++ template/helix-express.js
@@ -13,6 +13,7 @@ var unicorn = require("./unicorn.js");
 var habitat = require("./habitat.js");
 var helix = require("./helix.js");
 var del = require('del');
+var replace = require('gulp-string-replace');
 
 var config;
 if (fs.existsSync('../gulp-config.js.user')) {
@@ -31,9 +32,10 @@ gulp.task("express", function (callback) {
         "02-Nuget-Restore",
         "Express-03-Publish-All-Projects",
         "04-Apply-Xml-Transform",
+		"clean:configs",
+        "Express-Publish-All-Configs",
         "05-Sync-Unicorn",
         "06-Deploy-Transforms",
-        "Publish-All-Configs",
         callback);
 });
 
@@ -46,9 +48,10 @@ gulp.task("Express-03-Publish-All-Projects", function (callback) {
 var publishExpressProjects = function (location, dest) {
     dest = dest || config.websiteRoot;
     console.log("publish to " + dest + " folder");
+	console.log("location to " + location + " folder");
     return gulp.src([location + "*.Express.csproj"])
         .pipe(foreach(function (stream, file) {
-            return publishStream(stream, dest);
+            return expressPublishStream(stream, dest);
         }));
 };
 
@@ -77,6 +80,85 @@ gulp.task("Express-Build-Solution", function () {
 });
 
 gulp.task("Express-Publish-All-Projects", function () {
-    return publishExpressProjects(".");
+    return publishExpressProjects("./");
+});
+
+gulp.task("Express-Publish-Assemblies", function () {
+    var root = "./src";
+
+    var binFiles = [
+        root + "/bin/Sitecore.*.Express.*.{dll,pdb}",
+        root + "/**/code/**/bin/Sitecore.*.Express.*.{dll,pdb}"
+    ];
+
+    var destination = config.websiteRoot + "/bin/";
+    return gulp.src(binFiles, { base: root })
+        .pipe(rename({ dirname: "" }))
+        .pipe(newer(destination))
+        .pipe(debug({ title: "Copying " }))
+        .pipe(gulp.dest(destination));
+});
+
+var expressPublishStream = function (stream, dest) {
+  var targets = ["Build"];
+
+  return stream
+    .pipe(debug({ title: "Building project:" }))
+    .pipe(msbuild({
+      targets: targets,
+      configuration: config.buildConfiguration,
+      logCommand: false,
+      verbosity: config.buildVerbosity,
+      stdout: true,
+      errorOnFail: true,
+      maxcpucount: config.buildMaxCpuCount,
+      nodeReuse: false,
+      toolsVersion: config.buildToolsVersion,
+      properties: {
+        Platform: config.publishPlatform,
+        DeployOnBuild: "true",
+        DeployDefaultTarget: "WebPublish",
+        WebPublishMethod: "FileSystem",
+        DeleteExistingFiles: "false",
+        publishUrl: dest,
+        _FindDependencies: "false"
+      }
+    }));
+}
+
+gulp.task("express-pac", function (callback) {
+    config.runCleanBuilds = true;
+    return runSequence(
+		"clean:configs",
+        "Express-Publish-All-Configs",
+        callback);
+});
+
+gulp.task("Express-Publish-All-Configs", function () {
+  var root = "./src";
+  var roots = [root + "/**/App_Config", "!" + root + "/**/obj/**/App_Config"];
+  var files = "/**/*.config";
+  var destination = config.websiteRoot + "\\App_Config";
+  return gulp.src(roots, { base: root }).pipe(
+    foreach(function (stream, file) {
+      console.log("Publishing from " + file.path);
+      gulp.src(file.path + files, { base: file.path })
+        .pipe(replace(', '+  config.companyPrefix +'.Foundation.*\']', ', Sitecore.Foundation.Express\']'))
+		.pipe(replace(', '+  config.companyPrefix +'.Feature.*\]', ', Sitecore.Feature.Express\']'))
+		.pipe(replace(', '+  config.companyPrefix +'.Foundation.*"', ', Sitecore.Foundation.Express"'))
+		.pipe(replace(', '+  config.companyPrefix +'.Feature.*"', ', Sitecore.Feature.Express"'))
+		
+		.pipe(replace(', '+  config.companyPrefix +'.Feature.*<', ', Sitecore.Feature.Express<'))
+		.pipe(replace(', '+  config.companyPrefix +'.Foundation.*<', ', Sitecore.Foundation.Express<'))
+		
+		
+		
+		.pipe(newer(destination))
+        .pipe(debug({ title: "Copying " }))
+		
+        .pipe(gulp.dest(destination));
+      return stream;
+    })
+  );
 });
 
