4ddf460b71145cb1a36a29c935c04067f5f324ce
Thomas Tyack <thomastyack@aceik.com.au>
2017-09-19 01:04:21 +1000

    Better namespace filtering

--- template/helix-express.js
+++ template/helix-express.js
@@ -143,15 +143,14 @@ gulp.task("Express-Publish-All-Configs", function () {
     foreach(function (stream, file) {
       console.log("Publishing from " + file.path);
       gulp.src(file.path + files, { base: file.path })
-        .pipe(replace(', '+  config.companyPrefix +'.Foundation.*\']', ', Sitecore.Foundation.Express\']'))
-		.pipe(replace(', '+  config.companyPrefix +'.Feature.*\]', ', Sitecore.Feature.Express\']'))
-		.pipe(replace(', '+  config.companyPrefix +'.Foundation.*"', ', Sitecore.Foundation.Express"'))
-		.pipe(replace(', '+  config.companyPrefix +'.Feature.*"', ', Sitecore.Feature.Express"'))
+      	.pipe(replace(', '+  config.companyPrefix +'.Feature\.*<', ', Sitecore.Feature.Express<'))
+		.pipe(replace(', '+  config.companyPrefix +'.Foundation\.*<', ', Sitecore.Foundation.Express<'))
 		
-		.pipe(replace(', '+  config.companyPrefix +'.Feature.*<', ', Sitecore.Feature.Express<'))
-		.pipe(replace(', '+  config.companyPrefix +'.Foundation.*<', ', Sitecore.Foundation.Express<'))
+		.pipe(replace(', '+  config.companyPrefix +'\.Foundation\.([a-zA-Z].)*"', ', Sitecore.Foundation.Express"'))
+		.pipe(replace(', '+  config.companyPrefix +'\.Feature\.([a-zA-Z].)*"', ', Sitecore.Feature.Express"'))
 		
-		
+		.pipe(replace(', '+  config.companyPrefix +'\.Foundation\.*\'\]', ', Sitecore.Foundation.Express\'\]'))
+		.pipe(replace(', '+  config.companyPrefix +'\.Feature\.*\'\]', ', Sitecore.Feature.Express\'\]'))
 		
 		.pipe(newer(destination))
         .pipe(debug({ title: "Copying " }))
